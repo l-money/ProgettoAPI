2a016aad9a70e50789e97c190eba135f11f5766b
lorenzo <lorenzo.moneta@mail.polimi.it>
2020-06-10 11:08:05 +0200

    Cominciato a testare undo Riempio la stack di redo Devo ancora gestire redo

--- main.c
+++ main.c
@@ -3,42 +3,124 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int current_size = 1;
+/*Non c'è la riga 0*/
+typedef struct {
+    int from;
+    int to;
+    char event;
+    char **bkp;
+    struct status *prew;
+} stack;
+
+int current_size = 0;
 char **testo;
+int undo_size = 0;
+int redo_size = 0;
+stack *undo_stack = NULL;
+stack *redo_stack = NULL;
 
 /*Ricordati sta roba per non intasare la memoria*/
 void svuota_pila() {}
+
+void print_stack(stack *toPrint) {
+    do {
+        printf("From: %d\tTo: %d\n", toPrint->from, toPrint->to);
+        for (int i = 0; i < (toPrint->to - toPrint->from); i++) {
+            printf("?? %d %d\n", toPrint->from, toPrint->to);
+            printf("B %d < %d\n", i, (toPrint->to - toPrint->from));
+            if (toPrint->bkp[i] == NULL) {
+                printf(".\n");
+            } else {
+                printf("%s\n", toPrint->bkp[i]);
+            }
+        }
+        toPrint = toPrint->prew;
+    } while (toPrint != NULL);
+}
 
 /*Stampa lo stato del file di testo*/
 void print(int from, int to) {
     from--;
     to--;
     for (int i = from; i <= to; i++) {
-        if (i >= current_size) {
+        if (i >= current_size || i < 0) {
             printf(".\n");
         } else {
-            printf("%s\n", testo[i]);
+            printf("%s", testo[i]);
+            //printf("%d\n", i);
         }
     }
 }
 
+char **dump_backup(int from, int to) {
+    char **bkp;
+    bkp = malloc((to - from) * sizeof(char *));
+    //printf("from: %d\tTo: %d\n", from, to);
+    int j = 0;
+    for (int i = from; i < to; i++) {
+        if (bkp[j] == NULL) {
+            bkp[j] = malloc(1024 * sizeof(char));
+        }
+        strcpy(bkp[j], testo[i]);
+        j++;
+    }
+    return bkp;
+}
+
 /*Ad una modifica o cancellazione salva lo stato delle righe
  * intaccate all'interno dell'apposita struttura dati per l'undo*/
-void save_backup(char** bkp, char cmd, int from, int to){
-    printf("Salvataggio:\n");
-    printf("operazione:\t");
-    switch(cmd){
-        case 'c':
-            printf("c\n");
-            break;
-        case 'd':
-            printf("d\n");
-            break;
+void save_backup_undo(char **bkp, char cmd, int from, int to) {
+    //printf("Salvataggio:\n");
+    //printf("operazione:\t");
+    stack *old_stat = undo_stack;
+    undo_stack = malloc(sizeof(stack));
+    undo_stack->prew = old_stat;
+    undo_stack->from = from;
+    undo_stack->to = to;
+    undo_stack->event = cmd;
+    undo_stack->bkp = bkp;
+    undo_size++;
+    //printf("Old status from row %d to row %d\n", from, to);
+    for (int i = 0; i < (to - from); i++) {
+        //printf("%d  %s\n", from+i, bkp[i]);
     }
-    printf("Old status from row %d to row %d\n", from, to);
-    for(int i=0; i<(to-from); i++){
-        printf("%d  %s\n", from+i, bkp[i]);
+    /*Da togliere*/
+    //free(bkp);
+}
+
+void restore_backup(stack *stato) {
+    int j = stato->from;
+    for (int i = 0; i < (stato->to - stato->from); i++) {
+        free(testo[j]);
+        /*Se non va fare strcpy*/
+        testo[j] = stato->bkp[i];
+        j++;
     }
+    free(stato);
+}
+
+void save_backup_redo(int from, int to) {
+    stack *old_stat = redo_stack;
+    redo_stack = malloc(sizeof(stack));
+    redo_stack->prew = old_stat;
+    redo_stack->from = from;
+    redo_stack->to = to;
+    redo_stack->event = 'r';
+    redo_stack->bkp = dump_backup(from, to);
+    redo_size++;
+}
+
+void undo(int steps) {
+    stack *s = undo_stack;
+    for (int i = 0; s != NULL && i < steps; i++) {
+        save_backup_redo(s->from, s->to);
+        restore_backup(s);
+        s = s->prew;
+    }
+}
+
+void redo(int steps) {
+
 }
 
 /*Data la riga vuota (empty) shifta tutte le righe
@@ -53,27 +135,30 @@ void compress(int empty) {
  * - compress per compattare i "buchi"
  * - save_backup per salvare lo stato prima della cancellazione*/
 void delete(int from, int to) {
-    printf("%d > %d\n", from, current_size);
+    //printf("%d > %d\n", from, current_size);
     if (from > current_size) {
         return;
     }
     from--;
     char **bkp;
-    bkp = malloc((to - from + 1) * sizeof(char *));
     int j = 0;
     if (to > current_size) {
         to = current_size;
     }
+    bkp = dump_backup(from, to);
+
     for (int i = from; i < to; i++, j++) {
-        printf("riga: %d\tbkp: %d\n", i, j);
-        bkp[j] = malloc(1024 * sizeof(char));
-        strcpy(bkp[j], testo[from]);
+        //printf("riga: %d\tbkp: %d\n", i, j);
+        /*bkp[j] = malloc(1024 * sizeof(char));
+        strcpy(bkp[j], testo[from]);*/
         free(testo[from]);
         compress(from);
-        testo[current_size - 1] = NULL;
+        testo[current_size - 1] = NULL; //FREE?
         current_size--;
+
     }
-    save_backup(bkp, 'd', from, to);
+    save_backup_undo(bkp, 'd', from, to);
+    testo = realloc(testo, current_size * sizeof(char *));
 }
 
 /*Sostituisce il testo esistente in una riga con quello richiesto all'utente
@@ -81,32 +166,32 @@ void delete(int from, int to) {
  * se la riga esiste chiama save_backup per salvare il vecchio stato
  * salva anche se la riga vecchia era vuota e la etichetta NULL*/
 void change(int from, int to) {
+    char **bkp;
+    from--;
     if (to > current_size) {
+        bkp = dump_backup(from, current_size);
         testo = realloc(testo, to * sizeof(char *));
         current_size = to;
+    } else {
+        bkp = dump_backup(from, to);
     }
-    from--;
+
     char f[10];
-    gets(f);
-    char **bkp;
-    bkp = malloc((to - from + 1) * sizeof(char *));
-    int j=0;
+    fgets(f, 10, stdin);
+    int j = 0;
     for (int i = from; i <= to; i++, j++) {
         char c[1024];
-        gets(c);
+        fgets(c, 1024, stdin);
         if (strcmp(c, ".\0") == 0 || i == to) {
             break;
         } else {
             if (testo[i] == NULL) {
                 testo[i] = malloc(1024 * sizeof(char));
-            }else{
-                bkp[j] = malloc(1024 * sizeof(char));
-                strcpy(bkp[j], testo[i]);
-           }
+            }
             strcpy(testo[i], c);
         }
     }
-    save_backup(bkp, 'c', from, to);
+    save_backup_undo(bkp, 'c', from, to);
 
 }
 
@@ -119,14 +204,14 @@ bool interpreta(char *input) {
     int length = (int) strlen(input);
     char cmd;
     cmd = input[length - 1];
+    printf("cmd: %c\n", cmd);
     input[length - 1] = '\0';
     char *token = strtok(input, ",");
     int from = atoi(token);
+    printf("from: %d\n", from);
     int to;
+    token = strtok(NULL, ",");
     if (token != NULL) {
-        token = strtok(NULL, ",");
-        to = atoi(token);
-    } else {
         to = atoi(token);
     }
     printf("From: %d\tTo: %d\tCmd: %c\n", from, to, cmd);
@@ -141,6 +226,8 @@ bool interpreta(char *input) {
             delete(from, to);
             break;
         case 'u':
+            printf("Undo %d\n", from);
+            undo(from);
             break;
         case 'r':
             break;
@@ -151,15 +238,17 @@ bool interpreta(char *input) {
 }
 
 int main() {
-    testo = malloc(sizeof(char *));
     char *input;
     while (true) {
         scanf("%ms", &input);
         //printf("%s\n", input);
         if (input[0] == 'q') {
             break;
+        } else if (input[0] == 's') {
+            print_stack(undo_stack);
+        } else {
+            interpreta(input);
         }
-        interpreta(input);
     }
     return 0;
 }
